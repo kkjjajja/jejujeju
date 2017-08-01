146baac91776aeead2bbf5aae8f1ecbd3facb2cf
kkjjajja <30408145+kkjjajja@users.noreply.github.com>
2017-08-01 15:19:14 +0900

    -

--- js/app.js
+++ js/app.js
@@ -1,70 +1,21 @@
 $(document).ready(function() {
   var $heart = $(".heart");
-  $heart.click(function() {
-    // var hasHeartClass = $(this).hasClass("heart-clicked");
-    // if (hasHeartClass) {
-    //   $(this).removeClass("fa-heart");
-    //   $(this).removeClass("heart-clicked");
-    //   $(this).addClass("fa-heart-o");
-    // } else {
-    //   $(this).removeClass("fa-heart-o");
-    //   $(this).addClass("fa-heart");
-    //   $(this).addClass("heart-clicked");
-    // }
-     $(this).toggleClass("heart-clicked fa-heart fa-heart-o");
+  $heart.click(function(){
+    $(this).toggleClass("heart-clicked fa-heart fa-heart-o");
   });
+  var $profileOverlay = $(".profile__overlay");
+  var $closeBtn = $(".cancelLauncher");
 
-  var $cog = $(".fa-cog");
-  var $profile__overlay = $(".profile__overlay");
-  $cog.click(function() {
-      $profile__overlay.fadeIn();
-    // $(this).toggleClass("heart-clicked fa-heart fa-heart-o");
+  $closeBtn.click(function(){
+    $profileOverlay.fadeOut();
   });
+  var $openBtn = $(".fa-cog");
+  $openBtn.click(function(){
+    $profileOverlay.fadeIn();
+  })
 
-  var $times = $(".times");
-  $times.click(function() {
-    var HasTimesClass = $(this).hasClass("fa-times");
-    if (HasTimesClass) {
-      $(this).removeClass("fa-times");
-      $(this).removeClass("profile__overlay");
-      $(this).removeClass("profile__overlay-container");
-      $(this).removeClass("profile__overlay-link");
-    }
-    // $(this).toggleClass("heart-clicked fa-heart fa-heart-o");
-  });
-
-
-
+  var $logoutBtn = $(".logoutLauncher");
+  $logoutBtn.click(function(){
+    window.location = 'index.html'
+  })
 });
-
-// var weapons = ["Magic Wand", "The Legendary Sword","Hobbit Army"];
-// var soldier = {
-//   name: "coco",
-//   age: 2,
-//   weapon: weapons[2],
-//   isKorean: false,
-//   isDead: false,
-//   sayHi: function(){
-//     console.log("Hello, my names is" + this.name + "and I'm:" + this.age);
-//   }
-// };
-//
-// function multiply(aa,bb){
-//   return(aa*bb);
-// }
-//
-// function sum(a,b){
-//   return a+b;
-// }
-//
-// function substrac(c,d){
-//   return c-d;
-// }
-//
-// function divide(e,f){
-//   return e/f;
-// }
-//
-// function expo(g,h){
-//   return g**h;
-// }
